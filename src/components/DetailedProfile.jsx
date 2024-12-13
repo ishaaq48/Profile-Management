a88fd4ef6ce6fc30544a93a8e2fdba26ad34b29e
Syed Ishaaq <syedishaaq48@gmail.com>
2024-12-13 15:22:52 +0530

    Detailed-Profile-Layout

--- src/components/DetailedProfile.jsx
+++ src/components/DetailedProfile.jsx
@@ -5,30 +5,32 @@ export default function DetailedProfile() {
         profile
     } = useContext(ProfileContext)  
   return (
-    <>
+    <div className="container mt-4">
+      <div className="row">
         {profile.map(user => (
-  <div className="col-md-4" key={user.id}>
-    <div className="card" style={{ width: '30rem' }}>
-      <img className="card-img-top" src={user.image} alt={user.name} />
-      <div className="card-body">
-        <h3 className="card-title">{user.name}</h3>
-        <p className="card-text">{user.description}</p>
-        <p>Skills: {user.skills.join(', ')}</p>
-        <p>Experience: {user.experience}</p>
-        <p>Education: {user.education}</p>
-        <p>Hobbies: {user.hobbies}</p>
-        <p>
-          Social Links: 
-          <a href={user.socialLinks.linkedIn}>LinkedIn</a>, 
-          <a href={user.socialLinks.github}>GitHub</a>, 
-          <a href={user.socialLinks.twitter}>Twitter</a>
-        </p>
-        <button className="btn btn-primary">View Details</button>
+          <div className="col-lg-4 col-md-12 mb-4" key={user.id}>
+            <div className="card" style={{ width: '100%' }}>
+              <img className="card-img-top" src={user.image} alt={user.name} />
+              <div className="card-body">
+                <h3 className="card-title">{user.name}</h3>
+                <p className="card-text">{user.description}</p>
+                <p>Skills: {user.skills.join(', ')}</p>
+                <p>Experience: {user.experience}</p>
+                <p>Education: {user.education}</p>
+                <p>Hobbies: {user.hobbies}</p>
+                <p>
+                  Social Links: 
+                  <a href={user.socialLinks.linkedIn} className="ms-2">LinkedIn</a>, 
+                  <a href={user.socialLinks.github} className="ms-2">GitHub</a>, 
+                  <a href={user.socialLinks.twitter} className="ms-2">Twitter</a>
+                </p>
+                <button className="btn btn-primary">View Details</button>
+              </div>
+            </div>
+          </div>
+        ))}
       </div>
     </div>
-  </div>
-))}
-    </>
     
   )
 }
